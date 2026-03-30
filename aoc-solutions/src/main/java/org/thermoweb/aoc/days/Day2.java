91219c82e5afb4153591eea2ac869a00d9a8f6b3
Anton De Joya <antonjaie.dejoya@gmail.com>
2026-03-30 16:09:07 -0700

    Added answer for Day 2

--- aoc-solutions/src/main/java/org/thermoweb/aoc/days/Day2.java
+++ aoc-solutions/src/main/java/org/thermoweb/aoc/days/Day2.java
@@ -9,13 +9,80 @@ import org.thermoweb.aoc.DaySolver;
 
 @DaySolver(2)
 public class Day2 implements Day {
-    @Override
-    public Optional<BigInteger> partOne(String input) {
-        return Optional.empty();
+
+    private boolean isInvalidId(String input) {
+
+        if(input.length() % 2 != 0) {
+            return false;
+        }
+
+        int halfPoint = input.length() / 2;
+        String firstHalf = input.substring(0, halfPoint);
+        String secondHalf = input.substring(halfPoint);
+
+
+        return firstHalf.equals(secondHalf);
     }
 
     @Override
+    public Optional<BigInteger> partOne(String input) {
+        String[] ranges = input.split(",");
+
+        long result = 0;
+        for(String r : ranges) {
+            String[] range = r.split("-");
+
+            if(range[0].startsWith("0")) {
+                continue;
+            }
+            for(long id = Long.parseLong(range[0]); id <= Long.parseLong(range[1]); id++) {
+                if(isInvalidId(String.valueOf(id))) {
+                    result += id;
+                }
+            }
+        }
+
+        return Optional.of(BigInteger.valueOf(result));
+    }
+
+    private boolean isInvalidIDPart2(String input) {
+        for(int i = 1; i <= input.length() / 2; i++) {
+
+            if(input.length() % i != 0) continue;
+
+            int reps = input.length() / i;
+            if(reps < 2) continue;
+
+            String pattern = input.substring(0, i);
+            boolean isValid = true;
+
+            for(int j = 0; j < input.length(); j++) {
+                if(input.charAt(j) != pattern.charAt(j % i)) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid) return true;
+        }
+
+        return false;
+    }
+    @Override
     public Optional<BigInteger> partTwo(String input) {
-        return Optional.empty();
+        String[] ranges = input.split(",");
+
+        long result = 0;
+        for(String r : ranges) {
+            String[] range = r.split("-");
+
+            for(long id = Long.parseLong(range[0]); id <= Long.parseLong(range[1]); id++) {
+                if(isInvalidIDPart2(String.valueOf(id))) {
+                    result += id;
+                }
+            }
+        }
+
+        return Optional.of(BigInteger.valueOf(result));
     }
 }
--- aoc-solutions/src/test/java/org/thermoweb/aoc/days/Day2Test.java
+++ aoc-solutions/src/test/java/org/thermoweb/aoc/days/Day2Test.java
@@ -3,6 +3,7 @@ package org.thermoweb.aoc.days;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.lang.Exception;
+import java.math.BigInteger;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.thermoweb.aoc.Day;
@@ -13,11 +14,11 @@ class Day2Test {
 
     @Test
     void test_part_one() throws Exception {
-        assertEquals(Optional.empty(), day.partOne(DayRunner.getExample(2)));
+        assertEquals(Optional.of(BigInteger.valueOf(1227775554)), day.partOne(DayRunner.getExample(2)));
     }
 
     @Test
     void test_part_two() throws Exception {
-        assertEquals(Optional.empty(), day.partTwo(DayRunner.getExample(2)));
+        assertEquals(Optional.of(BigInteger.valueOf(4174379265L)), day.partTwo(DayRunner.getExample(2)));
     }
 }
